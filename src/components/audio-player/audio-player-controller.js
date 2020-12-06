d913a7cc3b998f68d7dfca1a1f8da9fb3cbe9811
Silvio Schmidt <silvio.schmidt@xing.com>
2020-12-06 22:51:37 +0100

    fix clean tone not playing with sweeps

--- src/components/audio-player/audio-player-controller.js
+++ src/components/audio-player/audio-player-controller.js
@@ -205,12 +205,12 @@ const AudioPlayerController = ({ presets = [], slug = "" }) => {
 
     if (isPedalOn) {
       activePresetId = activePreset.id
-    }
 
-    if (activePreset.isSweep) {
-      const { target, initialValue } = activePreset
-      const level = sweepSetting[target] || initialValue
-      activePresetId += `_${level}`
+      if (activePreset.isSweep) {
+        const { target, initialValue } = activePreset
+        const level = sweepSetting[target] || initialValue
+        activePresetId += `_${level}`
+      }
     }
 
     if (isPlaying) {
