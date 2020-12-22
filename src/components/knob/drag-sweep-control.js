7cfe77541907b43259cae85e5fff2c2def782a82
Silvio Schmidt <silvio.schmidt@xing.com>
2020-12-22 01:15:34 +0100

    revert last commit

--- src/components/knob/drag-sweep-control.js
+++ src/components/knob/drag-sweep-control.js
@@ -110,7 +110,7 @@ const DragSweepControl = ({ id = "", render = () => {}, size = 64 }) => {
     window.addEventListener("mouseup", () => {
       window.removeEventListener("mousemove", throttledHandleDrag)
     })
-    window.addEventListener("touchend touchcancel", () => {
+    window.addEventListener("touchend", () => {
       window.removeEventListener("touchmove", throttledHandleDrag)
     })
   }
