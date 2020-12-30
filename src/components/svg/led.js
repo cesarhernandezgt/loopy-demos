5e0ddb89f3b5945eddcb7a9e2475f6cf67e9bb26
Silvio Schmidt <silvio.schmidt@xing.com>
2020-12-30 22:23:51 +0100

    finalize demo content

--- src/components/svg/led.js
+++ src/components/svg/led.js
@@ -1,4 +1,21 @@
 import React from "react"
+import styled from "styled-components"
+
+const AnimationContainer = styled.g`
+  animation: blinking ${props => props.time}ms ease-in-out infinite;
+
+  @keyframes blinking {
+    0% {
+      opacity: 1;
+    }
+    50% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+`
 
 const Led = ({
   isOn = false,
@@ -10,6 +27,8 @@ const Led = ({
     off: "#800000a1",
     socket: "gray",
   },
+  isBlinking = false,
+  blinkTime = 0,
 }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
@@ -31,12 +50,12 @@ const Led = ({
       ) : (
         <circle cx="24" cy="24" r="8" fill={colors.off} />
       )}
-      {isOn && (
-        <>
+      {(isOn || isBlinking) && (
+        <AnimationContainer fill="none" time={blinkTime}>
           <circle cx="24" cy="24" r="8" fill={colors.on} fillOpacity=".8" />
           <circle cx="24" cy="24" r="16" fill={colors.on} fillOpacity=".2" />
           <circle cx="24" cy="24" r="24" fill={colors.on} fillOpacity=".4" />
-        </>
+        </AnimationContainer>
       )}
     </g>
   </svg>
