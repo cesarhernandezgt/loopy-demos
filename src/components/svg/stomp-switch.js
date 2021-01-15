6efb85cfa7b2257c4459550d45ffa2c455048abb
Silvio Schmidt <silvio.schmidt@xing.com>
2021-01-15 22:20:44 +0100

    animate stomp switch down

--- src/components/svg/stomp-switch.js
+++ src/components/svg/stomp-switch.js
@@ -1,37 +1,69 @@
-import React from "react"
+import React, { useState } from "react"
 
-const StompSwitch = ({ onClick = () => {}, id = "", size = 64 }) => (
-  <button type="button" onClick={onClick} id={id} style={{ margin: 0 }}>
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width={size}
-      height={size}
-      viewBox="0 0 52 52"
+const StompSwitch = ({ onClick = () => {}, id = "", size = 64 }) => {
+  const [isDown, setIsDown] = useState(false)
+
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      id={id}
+      style={{ margin: 0 }}
+      onMouseDown={() => {
+        setIsDown(true)
+      }}
+      onTouchStart={() => {
+        setIsDown(true)
+      }}
+      onMouseUp={() => {
+        setIsDown(false)
+      }}
+      onTouchEnd={() => {
+        setIsDown(false)
+      }}
     >
-      <defs>
-        <radialGradient id="gradient1">
-          <stop offset="0%" stopColor="aliceblue" />
-          <stop offset="70%" stopColor="aliceblue" />
-          <stop offset="95%" stopColor="lightslategray" />
-        </radialGradient>
-        <radialGradient id="gradient2">
-          <stop offset="0%" stopColor="aliceblue" />
-          <stop offset="80%" stopColor="aliceblue" />
-          <stop offset="100%" stopColor="#a3bbd2" />
-        </radialGradient>
-      </defs>
-      <g fill="none">
-        <circle cx="26" cy="26" r="26" fill="url(#gradient1)" />
-        <polygon
-          fill="url(#gradient1)"
-          stroke="slategray"
-          points="32 8 52.785 20 52.785 44 32 56 11.215 44 11.215 20"
-          transform="rotate(90 32 32) translate(-6, 6)"
-        />
-        <circle cx="26" cy="26" r="16" fill="url(#gradient2)" />
-      </g>
-    </svg>
-  </button>
-)
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={size}
+        height={size}
+        viewBox="0 0 52 52"
+      >
+        <defs>
+          <radialGradient id="gradient1">
+            <stop offset="0%" stopColor="aliceblue" />
+            <stop offset="70%" stopColor="aliceblue" />
+            <stop offset="95%" stopColor="lightslategray" />
+          </radialGradient>
+          <radialGradient id="gradient2">
+            <stop offset="0%" stopColor="aliceblue" />
+            <stop offset="80%" stopColor="aliceblue" />
+            <stop offset="100%" stopColor="#a3bbd2" />
+          </radialGradient>
+        </defs>
+        <g fill="none">
+          <circle cx="26" cy="26" r="26" fill="url(#gradient1)" />
+          <polygon
+            fill="url(#gradient1)"
+            stroke="slategray"
+            points="32 8 52.785 20 52.785 44 32 56 11.215 44 11.215 20"
+            transform="rotate(90 32 32) translate(-6, 6)"
+          />
+          <circle
+            cx="26"
+            cy={`${isDown ? 27 : 26}`}
+            r={`${isDown ? 15 : 16}`}
+            fill="slategray"
+          />
+          <circle
+            cx="26"
+            cy={`${isDown ? 26 : 24}`}
+            r={`${isDown ? 15 : 16}`}
+            fill="url(#gradient2)"
+          />
+        </g>
+      </svg>
+    </button>
+  )
+}
 
 export default StompSwitch
