62aba8752869aa284843fcc377aa5f888a383f9c
Silvio Schmidt <silvio.schmidt@xing.com>
2021-02-26 11:50:14 +0100

    implement preset carousel

--- src/components/preset.js
+++ src/components/preset.js
@@ -1,5 +1,5 @@
-import React from "react"
-import styled, { css } from "styled-components"
+import React, { useRef } from "react"
+import styled from "styled-components"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import {
   faSpinner,
@@ -8,13 +8,17 @@ import {
 import useDemoState from "../helpers/use-demo-state"
 
 const StyledPresetTag = styled.button`
+  flex: 1 0 auto;
+  height: 42px;
+  scroll-snap-align: start;
+
   background-color: ${props => (props.isSweep ? "var(--pink)" : "var(--cyan)")};
   color: black;
   border-radius: 3px;
   font-size: 1rem;
   font-weight: 700;
   padding: 0.5rem;
-  margin: 0 0.25rem 0.25rem 0;
+  margin-right: 0.25rem;
   cursor: ${props => (props.isLoading ? "progress" : "pointer")};
   opacity: ${props => (props.active || props.hasError ? 1 : 0.4)};
 
@@ -60,6 +64,8 @@ const Preset = ({ id = "", label = "", isSweep = false }) => {
   const loaded = presetsLoaded.includes(id)
   const hasError = presetLoadingErrors.includes(id)
 
+  const itemRef = useRef(null)
+
   const renderIcon = () => {
     if (hasError) {
       return <FontAwesomeIcon icon={faExclamationTriangle} />
@@ -72,6 +78,21 @@ const Preset = ({ id = "", label = "", isSweep = false }) => {
     return null
   }
 
+  const handleClick = () => {
+    if (isActive) {
+      setIsPedalOn(false)
+    } else {
+      selectPreset(id)
+      setIsPedalOn(true)
+    }
+
+    itemRef.current.scrollIntoView({
+      behavior: "smooth",
+      inline: "center",
+      block: "nearest",
+    })
+  }
+
   return (
     <StyledPresetTag
       active={isActive}
@@ -80,14 +101,8 @@ const Preset = ({ id = "", label = "", isSweep = false }) => {
       type="button"
       disabled={!loaded || hasError}
       isLoading={!loaded && hasLoadingStarted}
-      onClick={() => {
-        if (isActive) {
-          setIsPedalOn(false)
-        } else {
-          selectPreset(id)
-          setIsPedalOn(true)
-        }
-      }}
+      onClick={handleClick}
+      ref={itemRef}
     >
       <span>{label}</span>
       {renderIcon()}
--- src/components/presets.js
+++ src/components/presets.js
@@ -5,21 +5,25 @@ import Preset from "./preset"
 
 const StyledPresetsContainer = styled.div`
   display: flex;
-  flex-flow: row wrap;
+  flex-flow: row nowrap;
   align-items: center;
+  height: 42px;
+
+  scroll-snap-type: x mandatory;
+  scroll-behavior: smooth;
+  overflow-x: auto;
+  scrollbar-width: 0;
+  scrollbar-color: transparent transparent;
+
+  ::-webkit-scrollbar {
+    display: none;
+  }
 `
 
 const Presets = ({ presets = [] }) => (
   <StyledPresetsContainer>
-    {presets.map(({ label, settings, id, isSweep, isHidden }) => (
-      <Preset
-        key={id}
-        id={id}
-        label={label}
-        isSweep={isSweep}
-        isHidden={isHidden}
-        settings={settings}
-      />
+    {presets.map(({ label, id, isSweep }) => (
+      <Preset key={id} id={id} label={label} isSweep={isSweep} />
     ))}
   </StyledPresetsContainer>
 )
